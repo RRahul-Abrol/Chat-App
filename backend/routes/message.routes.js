a98d536eb14b6fcd6692c1a62edef16c65f4f633
Rahul Abrol <rahulabrol@Mac.lan>
2025-10-07 18:02:07 +0530

    deployment

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -4,6 +4,7 @@ import  protectRoute  from "../middleware/protectRoute.js";
 
 const router=express.Router();
 router.get("/:id",protectRoute,getMessages); // Assuming you want to get messages for a specific conversation
-router.post("/send/:id",protectRoute,sendMessage); 
+router.post("/send/:id",protectRoute,sendMessage);
+
 
 export default router;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server:{
-    port:3000,
-    proxy:{
-      "/api":{
-        target:"http://localhost:8000",
-         changeOrigin: true, // Crucial for virtual hosting and necessary for Express
-        secure: false,      // Allows proxying to the local HTTP server
-    },
-  },
-},
+	plugins: [react()],
+	server: {
+		port: 3000,
+		proxy: {
+			"/api": {
+				target: "http://localhost:8000",
+			},
+		},
+	},
 });
